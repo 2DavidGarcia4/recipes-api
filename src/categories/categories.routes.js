be3844b1b9ebd16cfab9fbc9c1cef50c63ed33b3
2DavidGarcia4 <gacub20ru@gmail.com>
2022-10-30 12:36:40 -0600

    add autenticate and admin validate

--- src/categories/categories.routes.js
+++ src/categories/categories.routes.js
@@ -1,19 +1,28 @@
 const router = require("express").Router()
 const categoriesServices = require("./categories.services")
+const passport = require("passport")
+const { adminValidate } = require("../middlewares/role.midelware")
+require("../middlewares/auth.middleware")(passport)
 
 router.route('/')
   .get(
+    passport.authenticate('jwt', {session: false}),
     categoriesServices.getAllCategories
   )
   .post(
+    passport.authenticate('jwt', {session: false}),
+    adminValidate,
     categoriesServices.postCategory
   )
 
 router.route('/:id')
 .get(
+  passport.authenticate('jwt', {session: false}),
   categoriesServices.getCategoryById
 )
 .delete(
+  passport.authenticate('jwt', {session: false}),
+  adminValidate,
   categoriesServices.deleteCategory
 )
 
