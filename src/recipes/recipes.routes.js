a58db84b398e926736b45b1487d802c5564b2e4c
2DavidGarcia4 <gacub20ru@gmail.com>
2022-10-31 22:02:58 -0600

    add recipes routes

--- src/recipes/recipes.routes.js
+++ src/recipes/recipes.routes.js
@@ -1,7 +1,27 @@
 const router = require("express").Router()
 const services = require("./recipes.services")
+const passport = require("passport")
+const { adminValidate } = require("../middlewares/role.midelware")
+require("../middlewares/auth.middleware")(passport)
 
 router.route('/')
-
+  .get(
+    services.getAllRecipes
+  )
+  .post(
+    passport.authenticate('jwt', {session: false}),
+    services.postRecipes
+  )
 
 router.route('/:id')
+  .get(
+    services.getRecipeById
+  )
+  .patch(
+    passport.authenticate('jwt', {session: false}),
+    services.patchRecipe
+  )
+  .delete(
+    passport.authenticate('jwt', {session: false}),
+    services.deleteRecipe
+  )
